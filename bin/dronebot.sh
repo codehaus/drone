88b00c180680e8003a97bfddd0a6ee25bf59a4b9
gbevin <gbevin@0a77ca9b-11dd-0310-98fd-ecfae1ac51c2>
2004-06-17 21:51:38 +0000

    Added bin unix scripts

---
+++ bin/dronebot.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Copyright 2003 Geert Bevin <gbevin@uwyn.com>
+# Distributed under the terms of the GNU General Public License, v2 or later
+# $Id$
+
+PROGDIR="`dirname $0`"
+case "${PROGDIR}" in
+	[^/]*)
+		export PROGDIR="`pwd`/${PROGDIR}"
+		;;
+esac
+export PROGDIR="$PROGDIR/.."
+JETTY=$(ls -d $PROGDIR/jetty*)
+export JETTY_CONSOLE="$JETTY/logs/out.log"
+export JETTY_HOME="$JETTY"
+cd $JETTY_HOME
+java -classpath "$JETTY_HOME/lib/javax.servlet.jar:$JETTY_HOME/lib/org.mortbay.jetty.jar:$PROGDIR/lib/mysql-connector-java-3.0.11-stable-bin.jar:$PROGDIR/lib/postgres-jdbc-7.4.jar:$PROGDIR/lib/rife-0.7.2.jar:$PROGDIR/build/dist/drone-1.0.jar" com.uwyn.drone.Droned
